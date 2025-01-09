34ec71a97142d3c005f2074efcbfe66ed88d9f18
bboss204 <93838916+bboss204@users.noreply.github.com>
2025-01-09 19:38:40 +0100

    v test connection java

--- Java Code/Database.java
+++ Java Code/Database.java
@@ -1,3 +1,19 @@
+import java.sql.Connection;
+
 public class Database {
-    
+    String url;
+    String utilisateur;
+    String mdp;
+
+    public Database(String url, String utilisateur, String mdp){
+        this.url = url;
+        this.utilisateur = utilisateur;
+        this.mdp = mdp;
+    }
+
+    public static void connect(String[] args) {
+        Connection launchConnection = null;
+
+        
+    }
 }
